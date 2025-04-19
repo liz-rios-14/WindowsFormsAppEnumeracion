924044c876d576f9cac6bcb73f5385fa30cd8871
liz-rios-14 <rioslizbeth37@gmail.com>
2025-04-18 22:35:04 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsAppEnumeracion/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsAppEnumeracion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(26, 144);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(69, 16);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(29, 36);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(185, 24);
+            this.cmbEstado.TabIndex = 1;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(82, 82);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(86, 36);
+            this.btnMostrar.TabIndex = 2;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 219);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ WindowsFormsAppEnumeracion/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEnumeracion
+{
+    // Definimos una enumeración llamada EstadoPedido
+    // Representa los distintos estados que puede tener un pedido
+    public enum EstadoPedido
+    {
+        Pendiente,
+        Procesando,
+        Enviado,
+        Entregado,
+        Cancelado
+    }
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Al iniciar el formulario, cargamos los valores del enum EstadoPedido en el ComboBox
+            // Esto permite que el usuario seleccione un estado desde el menú desplegable
+            cmbEstado.DataSource = Enum.GetValues(typeof(EstadoPedido)); 
+        } 
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            // Obtenemos el estado seleccionado en el ComboBox y lo convertimos al tipo EstadoPedido
+            EstadoPedido estadoSeleccionado = (EstadoPedido)cmbEstado.SelectedItem;
+
+            // Evaluamos el estado con un switch para mostrar un mensaje correspondiente
+            switch (estadoSeleccionado)
+            {
+                case EstadoPedido.Pendiente:
+                    lblResultado.Text = "Tu pedido está pendiente.";
+                    break;
+                case EstadoPedido.Procesando:
+                    lblResultado.Text = "Tu pedido está siendo procesado.";
+                    break;
+                case EstadoPedido.Enviado:
+                    lblResultado.Text = "Tu pedido ha sido enviado.";
+                    break;
+                case EstadoPedido.Entregado:
+                    lblResultado.Text = "Tu pedido fue entregado.";
+                    break;
+                case EstadoPedido.Cancelado:
+                    lblResultado.Text = "Tu pedido fue cancelado.";
+                    break;
+                default:
+                    // En caso de seleccionar un valor no esperado 
+                    lblResultado.Text = "Estado desconocido.";
+                    break;
+            }
+
+        }
+    }
+}
